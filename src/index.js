447797398f7ebfefe5f169efe2a0e68bd08ff757
Bibliofile <bibliofilegit@gmail.com>
2018-03-09 23:26:36 -0700

    Use CodeMirror for mobile support

--- src/index.js
+++ src/index.js
@@ -6,46 +6,17 @@ const presets = {
     tabs: tabPreset
 }
 
-const input = document.querySelector('textarea')
 const iframe = document.querySelector('iframe')
-const errorContainer = document.querySelector('.footer')
 
-function debounce(ms, fn) {
-    let timeout = null
-    let savedArgs
-
-    return function (...args) {
-        savedArgs = args
-
-        clearTimeout(timeout)
-        timeout = setTimeout(() => {
-            timeout = null
-            fn(...savedArgs)
-        }, ms)
-    }
-}
-
-const editor = ace.edit("editor")
-editor.setTheme("ace/theme/github")
-editor.session.setMode("ace/mode/html")
-
-const session = editor.getSession()
-session.setUseWrapMode(true)
-session.on("changeAnnotation", function () {
-    const annotations = session.getAnnotations()
-    const filtered = annotations.filter(a => !a.text.includes('<!DOCTYPE html>'))
-
-    if (annotations.length != filtered.length) {
-        session.setAnnotations(filtered)
-    }
-});
-
-editor.on('change', debounce(1000, function () {
-    iframe.srcdoc = defaultSource
-        .replace('[CONTENT]', editor.getValue())
-
-    localStorage.setItem('userInput', editor.getValue())
-}))
+const editor = window.editor = CodeMirror(document.querySelector('#editor'), {
+    lineNumbers: true,
+    styleActiveLine: true,
+    matchBrackets: true,
+    lint: true,
+    gutters: ["CodeMirror-lint-markers"],
+    mode: "text/html"
+})
+editor.setSize("100%", "100%")
 
 const getQuery = () => new URLSearchParams(location.href.includes('?') ? location.href.substr(location.href.indexOf('?')) : '')
 
@@ -57,6 +28,9 @@ editor.setValue(starterInput)
 iframe.srcdoc = defaultSource
     .replace('[CONTENT]', starterInput)
 
+document.querySelector('button.run').addEventListener('click', () => {
+    iframe.srcdoc = defaultSource.replace('[CONTENT]', editor.getValue())
+})
 
 document.querySelector('button.share').addEventListener('click', () => {
     const query = getQuery()
